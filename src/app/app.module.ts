da447019dd4efb787b716baef124a24c7fd1330c
JavierHuertas <jhuertasozuna@gmail.com>
2017-07-20 10:55:57 -0600

    Clase Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ContactoService } from './services/ContactoService';
+import { AuthService } from './services/AuthService';
+
+import { ContactosPage } from '../pages/contactos/contactos';
+import { ContactoFormPage } from '../pages/contactos/contacto-form';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,26 +19,31 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ContactosPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ContactoFormPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ContactosPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ContactoFormPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthService,
+    ContactoService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
